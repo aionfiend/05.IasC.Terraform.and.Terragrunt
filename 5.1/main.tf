2cdb06af5f7cc10d2cbe19615e71a47fe1d74120
Aleksandr.Radvanski <aion.fiend@gmail.com>
2026-08-29 20:25:33 +0300

    5.1.2

--- 5.1/main.tf
+++ 5.1/main.tf
@@ -1,8 +1,9 @@
-resource  "random" "Pet" {
+resource  "random_pet" "Pet" {
   length = 2
   prefix = "work"
 }
 
 resource "local_file" "Pet_name" {
   filename = "~/Documents/DevOps_learning/05.Terraform/05.Iasc.Terraform.and.Terragrunt"
+  content  = "Имя моего питомца: ${random_pet.my_pet.id}\n"
 }
