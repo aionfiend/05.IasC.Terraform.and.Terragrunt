6d2a201b0e9d978bd2d147c7bde2d12b7c341c6e
Aleksandr.Radvanski <aion.fiend@gmail.com>
2026-08-31 15:53:17 +0300

    5.3.3

--- 5.1/outputs.tf
+++ 5.1/outputs.tf
@@ -1,8 +1,8 @@
-output "all_files_paths" {
-  value       = {for env, mod in module.config_files : env => mod. }
-  description = "пути создания файлов для всех сред"
+output "all_files_source" {
+  value       = {for env, mod in module.config_files : env => mod.source}
+  description = "source для всех сред"
 }
 output "all_pet_names" {
-  value = {for env, mod in module.config_files : env => mod. }
+  value = {for env, mod in module.config_files : env => mod.id }
   description = "Генерация имен животных для всех сред"
 }
